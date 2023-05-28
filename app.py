21ddb022969019ead24e8f777b5923784ec25731
s22020 <s22020@pjwstk.edu.pl>
2023-05-28 21:38:21 +0200

    Add basic functions, add base template

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "Hello world"
+def index(name):
+    return render_template('index.html')
+
+
+@app.route("/user/<name>")
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.route("/post")
+def post():
+    return render_template('post.html')
