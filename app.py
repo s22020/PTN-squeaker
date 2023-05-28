6ec6e1ff15cb7ef1a8fdd4ea28d954282938a0b3
s22020 <s22020@pjwstk.edu.pl>
2023-05-28 22:11:39 +0200

    Add bootstrap dependency

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap(app)
 
 @app.route("/")
-def index(name):
-    return render_template('index.html')
+def index():
+    return render_template('base.html')
 
 
 @app.route("/user/<name>")
