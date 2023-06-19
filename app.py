4da7f9ce23d9f2ace0d723843d45df4ba29c0bbd
s22020 <s22020@pjwstk.edu.pl>
2023-06-19 22:10:09 +0200

    Add database connection, add user and post models, add storing user and posts to database, add database initialization and migration scripts

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -5,9 +6,19 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'changeit'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -18,7 +29,13 @@ posts = []
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if User is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
     return render_template('user.html', form=form, name=session.get('name'))
 
@@ -26,9 +43,16 @@ def index():
 @app.route("/post", methods=["GET", "POST"])
 def post():
     form = PostForm()
-    if form.validate_on_submit():
-        posts.append(form.squeak.data.strip())
-        return redirect(url_for('post'))
+    user = User.query.filter_by(username=session.get('name')).first()
+    posts = Post.query.filter_by(user=user).all()
+    if user is not None:
+        print(user)
+        if form.validate_on_submit():
+            post = Post(post=(form.post.data.strip()))
+            db.session.add(post)
+            db.session.commit()
+            form.post.data = ''
+            return redirect(url_for('post'))
     return render_template('post.html', form=form, posts=posts, name=session.get('name'), current_time=datetime.utcnow())
 
 
@@ -43,10 +67,35 @@ def internal_server_error(e):
 
 
 class PostForm(FlaskForm):
-    squeak = TextAreaField('Squeak something', validators=[DataRequired()])
+    post = TextAreaField('Squeak something', validators=[DataRequired()])
     submit = SubmitField('Squeak')
 
 
 class NameForm(FlaskForm):
     name = StringField("What's your name?", validators=[DataRequired()])
     submit = SubmitField("Send")
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True, nullable=False)
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    post = db.Column(db.String(140), index=True, nullable=False)
+    likes = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Post=Post)
--- migrate.sh
+++ migrate.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export FLASK_APP=app.py
-
-flask db init
 flask db migrate
 flask db upgrade
 flask db stamp
