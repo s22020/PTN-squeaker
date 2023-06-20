46c304fd9ac9bf6c34d8fcc12193ca0eb7102f28
s22020 <s22020@pjwstk.edu.pl>
2023-06-20 14:09:16 +0200

    Add application emails sending

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -8,6 +9,7 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -15,13 +17,24 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'changeit'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['SQUEAKER_MAIL_SUBJECT_PREFIX'] = '[Squeaker]'
+app.config['SQUEAKER_MAIL_SENDER'] = 'Squeaker Admin <' + os.environ.get('SQUEAKER_MAIL_SENDER') + '>'
+
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+mail = Mail(app)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -75,6 +88,7 @@ class NameForm(FlaskForm):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, nullable=False)
     username = db.Column(db.String(64), unique=True, index=True, nullable=False)
     posts = db.relationship('Post', backref='user', lazy='dynamic')
 
@@ -96,3 +110,18 @@ class Post(db.Model):
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Post=Post)
+
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+def make_email_thread(to, subject, template, **kwargs):
+    msg = Message(app.config['SQUEAKER_MAIL_SUBJECT_PREFIX'] + subject,
+                    sender=app.config['SQUEAKER_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thrd = Thread(target=send_async_email, args=[app, msg])
+    thrd.start()
+    return thrd
--- initialize.sh
+++ initialize.sh
@@ -2,6 +2,11 @@
 
 export FLASK_APP=app.py
 
+export MAIL_USERNAME='changeit'
+export MAIL_PASSWORD='changeit'
+export SQUEAKER_MAIL_SENDER='changeit'
+export SQUEAKER_MAIL_SUBJECT_PREFIX='changeit'
+
 flask db init
 flask db migrate
 flask db upgrade
