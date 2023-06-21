8de53e9faa5de8b10328d6d11d7ac58f5a2224fa
s22020 <s22020@pjwstk.edu.pl>
2023-06-21 21:50:00 +0200

    Add migration upgrade

--- app/models.py
+++ app/models.py
@@ -72,6 +72,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     post = db.Column(db.String(140), index=True, nullable=False)
     likes = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
